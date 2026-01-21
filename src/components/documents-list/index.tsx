9690df470ac5a805e2cf696353a62cace236e423
Max Miracolo <max.miracolo@gmail.com>
2026-01-20 22:43:39 -0500

    move components into subfolders

--- src/components/documents-list/index.tsx
+++ src/components/documents-list/index.tsx
@@ -1,26 +1,31 @@
-'use client';
+// 'use client';
 
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { getDocuments } from '@/db/documents';
 import styles from './documents-list.module.css';
+export default async function DocumentsList() {
+  const documents = await getDocuments();
+  // const [documents, setDocuments] = useState<
+  //   { id: string; name: string; updatedAt: Date; createdAt: Date }[]
+  // >([]);
 
-export default function DocumentsList() {
-  const [documents, setDocuments] = useState<
-    { id: string; name: string; updatedAt: Date; createdAt: Date }[]
-  >([]);
+  // useEffect(() => {
+  //   getDocuments().then((documents) => {
+  //     setDocuments(documents || []);
+  //   });
+  // }, []);
 
-  useEffect(() => {
-    getDocuments().then((documents) => {
-      setDocuments(documents || []);
-    });
-  }, []);
+  if (!documents) {
+    return <h1 className={styles.h1}>No documents found</h1>;
+  }
 
   return (
-    <section className={styles.section}>
+    <>
       <h1 className={styles.h1}>Documents</h1>
       <div className={styles.tableHeader}>
         <span className={styles.tableHeaderCell}>Name</span>
+        <span className={styles.tableHeaderCell}>Tags</span>
         <span className={styles.tableHeaderCell}>Last Updated At</span>
         <span className={styles.tableHeaderCell}>Created At</span>
         <span className={styles.tableHeaderCell}>ID</span>
@@ -31,6 +36,13 @@ export default function DocumentsList() {
             <Link href={`/draw?documentId=${document.id}`}>{document.name}</Link>
           </span>
           <span className={styles.tableCell}>
+            {document.tags.map((tag) => (
+              <span className={styles.tag} key={tag}>
+                {tag}
+              </span>
+            ))}
+          </span>
+          <span className={styles.tableCell}>
             {new Date(document.updatedAt).toLocaleString('en-US')}
           </span>
           <span className={styles.tableCell}>
@@ -39,6 +51,6 @@ export default function DocumentsList() {
           <span className={styles.tableCell}>{document.id}</span>
         </div>
       ))}
-    </section>
+    </>
   );
 }
